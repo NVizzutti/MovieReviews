a77c410f8bd7b0b611ed95b8489f86c786ed548c
NVizzutti <njvizzutti@gmail.com>
2017-02-28 14:11:41 -0800

    build attempt

--- MovieReviews/App_Start/BundleConfig.cs
+++ MovieReviews/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace MovieReviews
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/bundles/tablesort.js").Include(
+            bundles.Add(new ScriptBundle("~/bundles/mybundle").Include(
                 "~/Scripts/tablesort.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
